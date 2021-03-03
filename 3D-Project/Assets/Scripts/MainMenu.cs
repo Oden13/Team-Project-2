67b9f483e60d55e366e0d12c2cda8f972f5db0d0
Peyton Paul <peyton.z.paul@gmail.com>
2021-03-03 00:47:12 -0500

    Main Menu Work 1

---
+++ 3D-Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+
+    public void SwitchScene(string SampleScene)
+{
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void quitgame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

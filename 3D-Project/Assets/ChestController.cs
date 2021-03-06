d0c10cb1e1d9b079a601fac0ac32cb4619edae8b
jthig613 <67336532+jthig613@users.noreply.github.com>
2021-03-06 15:25:35 -0500

    chest script

---
+++ 3D-Project/Assets/ChestController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    Animator anim;
+    private bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Pressed();
+            Debug.Log("Open Saysame");
+        }
+    }
+
+   void Pressed()
+    {
+ 
+                //This will set the bool the opposite of what it is.
+                isOpen = !isOpen;
+                
+ 
+                //This line will set the bool true so it will play the animation.
+                anim.SetBool("isOpen", !isOpen);
+    }
+}

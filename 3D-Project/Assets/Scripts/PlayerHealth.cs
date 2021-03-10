6d20a8a2b9a9ff3e6e06c903b27bd6b0bc20b1fe
jthig613 <67336532+jthig613@users.noreply.github.com>
2021-03-10 16:53:04 -0500

    healthbar

---
+++ 3D-Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    private float timeBtwDamage;
+    public Slider healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwDamage > 0)
+        {
+            timeBtwDamage -= Time.deltaTime;
+        }
+        healthBar.value = health;
+    }
+}

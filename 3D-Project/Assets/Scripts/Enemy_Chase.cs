986aa681af778d9638e22e897be36c53880d5e35
roneelee522 <roneelee522@hotmail.com>
2021-03-14 19:01:51 -0400

    camerachange

--- 3D-Project/Assets/Scripts/Enemy_Chase.cs
+++ 3D-Project/Assets/Scripts/Enemy_Chase.cs
@@ -8,16 +8,25 @@ public class Enemy_Chase : MonoBehaviour
     public GameObject player;
     public float range;
     public Transform playerDistance;
+    public float speedRotation = 1.0f;
 
    
     void Update () 
     {
         if(Vector3.Distance(playerDistance.position,transform.position) <= range)
         {
+         
+       
         Vector3 localPosition = player.transform.position - transform.position;
         localPosition = localPosition.normalized; // The normalized direction in LOCAL space
         transform.Translate(localPosition.x * Time.deltaTime * speed, 0, localPosition.z * Time.deltaTime * speed);
+
         }
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(playerDistance);
     }
 
     void OnTriggerEnter(Collider other)

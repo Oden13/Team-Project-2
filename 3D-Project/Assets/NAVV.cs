023f3f0b59daca824f039a887b72c81240d54285
roneelee522 <roneelee522@hotmail.com>
2021-03-15 07:42:56 -0400

    fixed the enemy chasing

---
+++ 3D-Project/Assets/NAVV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NAVV : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent nav;
+    public float range;
+    public Transform playerDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(playerDistance.position, transform.position) <= range)
+        {
+            nav.SetDestination(target.position);
+        }
+            
+    }
+}
